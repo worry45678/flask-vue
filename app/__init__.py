30f7a39fc01a7ada0636e2dd314b5e6cc4b9a0c1
worry45678 <worry45678@126.com>
2019-05-20 22:44:34 +0800

    去除中文

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,13 @@ client = pymongo.MongoClient(host='39.104.64.142', port=27017)
 db = client.fire
 db.authenticate('root','123456')
 
-def create_app(config_name):  # 注册app的函数
+def create_app(config_name):  
     """
     """
     app = Flask(__name__)
 
 
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint) # 注册蓝本
+    app.register_blueprint(main_blueprint) 
     
     return app
